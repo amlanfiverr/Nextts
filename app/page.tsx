9e2b04afc1057255d7feeb9a8d3409d91336d7b8
Lawrence-Amlan-Gomes <amlangomes@gmail.com>
2026-02-26 23:39:52 +0600

    Initial Next.js setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen font-bold bg-gray-50 text-[#111111] text-2xl flex justify-center items-center">
+      NextTs Test
+    </div>
+  );
+}
